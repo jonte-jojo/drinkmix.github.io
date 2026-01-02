dc8c3c753e54ffce9ee43f43582c7958453f56f1
jonjo899@student.liu.se <jonjo899@student.liu.se>
2026-01-02 23:35:51 +0100

    trigger deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/drinkmix.github.io/",
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
-  },
-  server: {
-    port: 5173,
-    host: true,
   },
 });

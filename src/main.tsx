a9f93b5e0500b4bd241022140f4b134b44180581
jonjo899@student.liu.se <jonjo899@student.liu.se>
2026-01-06 07:40:23 +0100

    Fix image paths with BASE_URL

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-
+console.log("ENV URL:", import.meta.env.VITE_SUPABASE_URL);
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter basename="/drinkmix.github.io">
